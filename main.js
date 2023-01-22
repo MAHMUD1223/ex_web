7a476818ee044ecfe54629107c50961119127505
MAHMUD1223 <mardhaka67@gmail.com>
2023-01-22 13:02:29 +0000

    created first app

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const express = require("express");
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res)=>{
+    res.send("This is a basic");
+})
+
+app.listen(port, () => {
+    console.log(`server started at ${port}`);
+})
